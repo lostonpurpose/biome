40519d10c92a660fb4017f8190ecfea42f58f0ec
Will VanderWyden <vanderwyden@gmail.com>
2025-05-19 17:58:44 +0900

    basic color change works

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const rangeSlider = document.querySelector("#oxygen");
 const oxygenLevel = document.querySelector(".oxy-level");
 
+
+
+
+const plants = document.querySelector(".plants");
+
 rangeSlider.addEventListener("input", function () {
-    oxygenLevel.textContent = "Current value: " + rangeSlider.value;
+    oxygenLevel.textContent = "Oxygen Level: " + rangeSlider.value;
+
+    const max = Number(rangeSlider.max) || 100; // fallback if max not set
+    const rangeValue = rangeSlider.value;
+    const color1 = 255 - Math.round((rangeValue / max) * 200); // 255 to 55
+    const color2 = 255 - Math.round((rangeValue / max) * 200); // 255 to 55
+
+    plants.style.backgroundColor = `rgb(${color1}, 255, ${color2})`
+    
   });
+
+
+
