079fb3d9398fbec211dc327b52839f4eb7583c45
Will VanderWyden <vanderwyden@gmail.com>
2025-05-19 17:36:15 +0900

    slider changes oxy level

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('log')
+const rangeSlider = document.querySelector("#oxygen");
+const oxygenLevel = document.querySelector(".oxy-level");
+
+rangeSlider.addEventListener("input", function () {
+    oxygenLevel.textContent = "Current value: " + rangeSlider.value;
+  });
